c2dc06b82895e239e0637be70dde9d7a9b576c03
juanfe9118 <1168@holbertonschool.com>
2019-12-10 21:36:53 +0000

    Modify fifth task added 0 in single digit numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(10):
         if i >= j:
             continue
         if i != 8 or j != 9:
-            print ("{:d}{:d}, " .format(i, j), end="")
+            print("{:d}{:d}, " .format(i, j), end="")
         if i == 8 and j == 9:
             print("{:d}{:d}" .format(i, j))
