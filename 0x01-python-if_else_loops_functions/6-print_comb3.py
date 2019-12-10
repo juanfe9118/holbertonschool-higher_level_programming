bf14a52d80c31daa9946c1a9c59ebd2e599251b0
juanfe9118 <1168@holbertonschool.com>
2019-12-10 21:21:04 +0000

    Modify seventh task, only use two print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,7 @@ for i in range(10):
     for j in range(10):
         if i >= j:
             continue
-        print ("{:d}{:d}" .format(i, j), end="")
         if i != 8 or j != 9:
-            print(", ", end="")
+            print ("{:d}{:d}, " .format(i, j), end="")
         if i == 8 and j == 9:
-            print()
+            print("{:d}{:d}" .format(i, j))
