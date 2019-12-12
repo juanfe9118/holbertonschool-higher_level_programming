4aa1c27a8836d1ae26cf4093cbbad8c1f95496de
juanfe9118 <1168@holbertonschool.com>
2019-12-12 21:50:04 +0000

    Modify import guard on 1st advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,25 +3,27 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 arlen = len(argv) - 1
-if arlen != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-
 a = int(argv[1])
 b = int(argv[3])
 
-if argv[2] == '+':
-    print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, add(a, b)))
+if __name__ == "__main__":
 
-elif argv[2] == '-':
-    print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, sub(a, b)))
+    if arlen != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-elif argv[2] == '*':
-    print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, mul(a, b)))
+    if argv[2] == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, add(a, b)))
 
-elif argv[2] == '/':
-    print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, div(a, b)))
+    elif argv[2] == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, sub(a, b)))
 
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    elif argv[2] == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, mul(a, b)))
+
+    elif argv[2] == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, div(a, b)))
+
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
