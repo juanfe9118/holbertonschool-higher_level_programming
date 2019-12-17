a1ebc94ad6b8ff749b8605208a43ca304799e39d
juanfe9118 <1168@holbertonschool.com>
2019-12-17 22:51:49 +0000

    Modify 4th task create list copy

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    lis = my_list.reverse()
+    for i in range(len(lis)):
+        print("{:d}".format(lis[i]))
