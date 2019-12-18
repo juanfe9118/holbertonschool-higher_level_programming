6db91499d56bb2df199b6c6a8dca54023bd2c35b
juanfe9118 <1168@holbertonschool.com>
2019-12-18 05:14:52 +0000

    Modify task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,14 @@
 
 
 def no_c(my_string):
-    for i in range(my_string.count('c')):
-        my_string.remove('c')
-    for i in range(my_string.count('C')):
-        my_string.remove('C')
+    my_list = []
+    for i in range(len(my_string)):
+        my_list.append(my_string[i])
+    for i in range(my_list.count('c')):
+        my_list.remove('c')
+    for i in range(my_list.count('C')):
+        my_list.remove('C')
+    new_string = ""
+    for i in range(len(my_list)):
+        new_string = new_string + my_list[i]
+    return new_string
