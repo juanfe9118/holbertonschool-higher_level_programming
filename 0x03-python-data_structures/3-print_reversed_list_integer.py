7612c3487e3211163c919d67f54e979bc2679b03
juanfe9118 <juan.buitrago@holbertonschool.com>
2019-12-18 13:58:27 -0500

    Modify when list is null

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list == []:
+        return
     my_list.reverse()
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
