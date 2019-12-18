f8f71d3a401e5c13b5789f055817fbfef571c700
juanfe9118 <juan.buitrago@holbertonschool.com>
2019-12-17 23:25:46 -0500

    Modify 6th task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 
 def no_c(my_string):
-    for i in range(my_string.count(c)):
-        my_string.remove(c)
-    for i in range(my_string.count(C)):
-        my_string.remove(C)
+    for i in range(my_string.count('c')):
+        my_string.remove('c')
+    for i in range(my_string.count('C')):
+        my_string.remove('C')
