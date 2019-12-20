cf334331a9718e34a25a8f2ef06e8f02a190cd2d
juanfe9118 <1168@holbertonschool.com>
2019-12-20 02:47:43 +0000

    Fix condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,10 +4,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    try:
-        key = list(a_dictionary.keys())
-    except:
-        return None
+    key = list(a_dictionary.keys())
     score = key[0]
     for i in range(len(key)):
         if a_dictionary[score] >= a_dictionary[key[i]]:
