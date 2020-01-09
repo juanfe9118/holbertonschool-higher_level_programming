9fe55a02309a9ee1a25026a50e214e5968ef0df0
juanfe9118 <1168@holbertonschool.com>
2020-01-09 22:11:57 +0000

    Modify task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Script that assigns a private instance attribute size to class Square with size validation"""
+
+
+class Square:
+    """Class that defines a square with a private instance attribute size"""
+
+    def __init__(self, size=0):
+        self.__size = size
