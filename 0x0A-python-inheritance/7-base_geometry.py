76b953ef9cc5b55271ab008e76fa8b8b179853cc
juanfe9118 <1168@holbertonschool.com>
2020-01-21 04:51:01 +0000

    Fix integer_validator and add rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Checks integer and correct value"""
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
