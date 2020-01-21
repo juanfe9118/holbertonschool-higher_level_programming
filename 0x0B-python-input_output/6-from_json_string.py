fbf6fa8c57e1597f47fa611eea4e943ef9d73b48
juanfe9118 <1168@holbertonschool.com>
2020-01-21 23:45:44 +0000

    Add first 9 tasks

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""From JSON string to object module"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string"""
+    return json.loads(my_str)
--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Create object from JSON file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an Object from a 'JSON file' and returns it"""
+    with open(filename, 'r') as fo:
+        return json.load(fo)
