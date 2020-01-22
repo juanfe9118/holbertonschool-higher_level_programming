4ebc18aa129e1087020f4789bc4d7eeab5c8208a
juanfe9118 <1168@holbertonschool.com>
2020-01-22 03:26:42 +0000

    Modify filename

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,10 +6,12 @@ from sys import argv
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
+filename = "add_item.json"
+
 try:
-    my_list = load_from_json_file("add_item.json")
+    my_list = load_from_json_file(filename)
 except:
     my_list = []
 for a in range(1, len(argv)):
     my_list.append(argv[a])
-save_to_json_file(my_list, "add_item.json")
+save_to_json_file(my_list, filename)
