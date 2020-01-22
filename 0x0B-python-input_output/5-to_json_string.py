c243387557bc733396f199d8593de3e9add412c8
juanfe9118 <1168@holbertonschool.com>
2020-01-22 03:18:21 +0000

    Add 10th task and modify import

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """To JSON string module"""
-import json
 
 
 def to_json_string(my_obj):
     """Returns the JSON representation of an object (string)"""
+    import json
     return json.dumps(my_obj)
--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Load, add, save module"""
+
+
+from sys import argv
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except:
+    my_list = []
+for a in range(1, len(argv)):
+    my_list.append(argv[a])
+save_to_json_file(my_list, "add_item.json")
