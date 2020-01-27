bbba0d4960f46048d4d9a90d725d37bc48b860f3
juanfe9118 <1168@holbertonschool.com>
2020-01-27 00:31:30 -0500

    Update first two classes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Base Class Module"""
+
+
+class Base:
+    """Class Base that will be the parent all the classes in the project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,51 @@
 #!/usr/bin/python3
+"""Rectangle Class Module"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle that defines a rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Getter and setter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter and setter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter and setter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter and setter for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
