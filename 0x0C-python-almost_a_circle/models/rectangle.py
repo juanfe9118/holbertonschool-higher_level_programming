b66f2c1649c12f704af1a313e7643d5ab08f107a
juanfe9118 <1168@holbertonschool.com>
2020-01-27 20:56:34 -0500

    Update rectangle dictionary definition

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,3 +110,8 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of the rectangle instance"""
+        return {'width': self.__width, 'height': self.__height, 'x': self.__x,
+                'y': self.__y, 'id': self.id}
