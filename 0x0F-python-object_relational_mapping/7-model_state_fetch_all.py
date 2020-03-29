7243d03e47f4211dcae3325f9ecfb1427a197e9c
juanfe9118 <1168@holbertonschool.com>
2020-03-29 00:55:04 -0500

    Add 8th task

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Lists all the states"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    db = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,)
+    res = db.execute('SELECT id, name FROM states ORDER BY id ASC;')
+    for item in res:
+        print('{}: {}'.format(item[0], item[1]))
