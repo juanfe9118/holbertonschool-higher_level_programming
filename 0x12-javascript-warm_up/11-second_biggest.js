90b249afa6f2a207ff17a56d0c0770c9e3470378
juanfe9118 <1168@holbertonschool.com>
2020-05-06 01:33:26 -0500

    Add second biggest script

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+args = process.argv;
+if (args.length <= 3){
+    console.log(0);
+} else {
+  let arr = []
+  for (let i = 2; i < args.length; i++){
+    arr.push(args[i]);
+  }
+  arr.sort(function(a, b){return b - a});
+  console.log(arr[1]);
+}
