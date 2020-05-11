5159473884422b16ba840848d063301e9387b973
juanfe9118 <1168@holbertonschool.com>
2020-05-11 17:36:43 -0500

    Add Rectangle class with constructor

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Rectangle class that has a constructor with height and width
+class Rectangle {
+  constructor (h, w) {
+    this.height = h;
+    this.width = w;
+  }
+}
+
+module.exports = Rectangle;
