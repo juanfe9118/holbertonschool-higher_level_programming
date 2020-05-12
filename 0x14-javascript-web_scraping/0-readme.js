cc11ef919773de571f286431c576dfaafe0cf5ba
juanfe9118 <1168@holbertonschool.com>
2020-05-12 16:43:10 -0500

    Change printing method

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const fileName = process.argv[2];
 
-fs.readFile(fileName, 'utf8', function (err, data) {
+fs.readFile(fileName, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
