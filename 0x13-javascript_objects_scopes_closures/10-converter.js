7bdb5f5b53cf9aa21627542490cd8f941f2dc86e
juanfe9118 <1168@holbertonschool.com>
2020-05-11 23:23:03 -0500

    Modify Converter for more readability

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 // Function that converts a number from base 10 to another base passed as argument
 exports.converter = function (base) {
-  return function (n) {
+  function convert (n) {
     return n.toString(base);
   };
+  return convert;
 };
