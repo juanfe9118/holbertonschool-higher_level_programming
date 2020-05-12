e7e3b0f093ea776c951e42d020450f4da8b2c701
juanfe9118 <1168@holbertonschool.com>
2020-05-12 16:23:41 -0500

    Change name of path

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Reads and prints the content of a file.
 const fs = require('fs');
-const path = process.argv[2];
+const fileName = process.argv[2];
 
-fs.readFile(path, 'utf-8', function (err, data) {
+fs.readFile(fileName, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
