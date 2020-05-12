9e1235da3e1c21a86e0c323f3e141cd83cbe3cba
juanfe9118 <1168@holbertonschool.com>
2020-05-12 16:35:21 -0500

    Modify back to previous version

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const fileName = process.argv[2];
 
-fs.readFile(fileName, 'utf-8', function (err, data) {
+fs.readFile(fileName, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
