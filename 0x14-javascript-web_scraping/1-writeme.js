226fb4e98349db9acccdcd2f0c4997faa20ecb46
juanfe9118 <1168@holbertonschool.com>
2020-05-12 17:45:24 -0500

    Add write file script

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 // Writes a string to a file.
+const fs = require('fs');
+const fileName = process.argv[2];
+const fileText = process.argv[3];
+
+fs.writeFile(fileName, fileText, (err) => {
+    if (err) {
+        console.log(err);
+    }
+})
