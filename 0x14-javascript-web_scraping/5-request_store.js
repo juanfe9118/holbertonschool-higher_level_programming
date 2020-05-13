c3e1be32c80495f7b18b2c8a370e3d554bc19362
juanfe9118 <1168@holbertonschool.com>
2020-05-12 22:38:38 -0500

    Fix semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,12 +6,12 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(filePath, body, (err) => {
+    if (err) {
+      console.log(err);
     }
-    fs.writeFile(filePath, body, (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
+  });
 });
